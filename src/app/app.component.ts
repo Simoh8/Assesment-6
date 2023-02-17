06bf3125536fc561e986e8ce308b6b6e65b4b019
simon muturi <simomutu@gmail.com>
2023-02-17 17:15:06 +0300

    the student angular app

---
+++ src/app/app.component.ts
@@ -0,0 +1,27 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'Assesment6';
+
+  data:{name:string,balance:string,details:string}[]=[]
+
+
+  addStudent(name:HTMLInputElement,balance:HTMLInputElement,details:HTMLInputElement){
+
+    this.data.push({name:name.value,balance:balance.value,details:details.value})
+  }
+  deleteStudent(index:number){
+    this.data.splice(index,1)
+  }
+  updateStudent(index:number){
+    this.data[index].name=name.value,
+    this.data[index].balance=balance.value
+    this.data[index].details=details.value
+  }
+
+}
